86a6fb044c1203b7fd505f9e3f57e96ce389cc98
deepu1067 <dshahadat10@gmail.com>
2024-02-16 22:54:58 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import uiuLogo from "./uiu.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  
+
+  const [msg, setMsg] = useState(null);
+
+  let entered = (event) => {
+    if (event.key === "Enter") {
+      // setMsg(event.target.value);
+      let value = event.target.value; //011202037
+      let batch = parseInt(value[3] + value[4] + value[5]);
+      
+      if(batch >= 191 && batch <= 202)
+        setMsg("Congo Mamu!!🥳")
+      else{
+        setMsg("Dukkhito!!😭")
+      }
+
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="">
+      <img src={uiuLogo}></img>
+      <h1>7th Convocation</h1>
+      <h3>24 February, 2024</h3>
+      <input
+        className="input"
+        name="studentId"
+        placeholder="Enter Your Id: "
+        onKeyDown={entered}
+      />
+      <p>{msg}</p>
+    </section>
   );
 }
 
