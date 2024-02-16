4ef39d0d8b0c4615e4c2921cfb29fca1d6c9b5e1
deepu1067 <dshahadat10@gmail.com>
2024-02-17 00:06:39 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import uiuLogo from "./uiu.png";
 import "./App.css";
 import { useState } from "react";
+import { ShowMessage } from "./show";
 
 function App() {
-  
-
-  const [msg, setMsg] = useState(null);
-
+  const [msg, setMsg] = useState("");
   let entered = (event) => {
     if (event.key === "Enter") {
-      // setMsg(event.target.value);
       let value = event.target.value; //011202037
       let batch = parseInt(value[3] + value[4] + value[5]);
-      
-      if(batch >= 191 && batch <= 202)
-        setMsg("Congo Mamu!!🥳")
-      else{
-        setMsg("Dukkhito!!😭")
-      }
 
+      if (batch >= 191 && batch <= 202) setMsg("Congo Mamu!!🥳");
+      else {
+        setMsg("Dukkhito!!😭");
+      }
     }
   };
 
   return (
     <section className="">
-      <img src={uiuLogo}></img>
+      <img src={uiuLogo} alt="uiu"></img>
       <h1>7th Convocation</h1>
       <h3>24 February, 2024</h3>
       <input
@@ -33,7 +28,7 @@ function App() {
         placeholder="Enter Your Id: "
         onKeyDown={entered}
       />
-      <p>{msg}</p>
+      <ShowMessage message = {msg} />
     </section>
   );
 }
